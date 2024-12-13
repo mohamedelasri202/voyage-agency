98573e78bd57b28bb10517143d17b9748b337d77
mohamedelasri202 <el7994995@gmail.com>
2024-12-13 18:32:58 +0100

    les fonctionalities de js

---
+++ main.js
@@ -0,0 +1,31 @@
+function showformreserve (){
+    var  x = document.getElementById("reserve")
+    if (x.style.display == "none"){
+        x.style.display ="block"
+
+    }else {
+        x.style.display="none"
+    }
+}
+function showformeact(){
+    var  x = document.getElementById("active")
+    if (x.style.display == "none"){
+        x.style.display ="block"
+
+    }else {
+        x.style.display="none"
+    }
+}
+ 
+function showformeaclient(){
+    var  x = document.getElementById("Mclient")
+    if (x.style.display == "none"){
+        x.style.display ="block"
+
+    }else {
+        x.style.display="none"
+    }
+}
+ 
+  
+ 

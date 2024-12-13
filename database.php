4373df9b2fb05a7f536cd4368bc99c7bcd28176a
mohamedelasri202 <el7994995@gmail.com>
2024-12-13 18:32:06 +0100

    connection de database et  fonction de l'ajoute sur database pout  les client

---
+++ database.php
@@ -0,0 +1,15 @@
+
+<?php
+ $host = 'localhost';
+ $user = 'root';
+ $pass = '';
+ $dbname = 'voyage';
+ $db = mysqli_connect($host, $user, $pass, $dbname);
+
+//  if ($db == false) {
+//      echo 'siir fhallk';
+//  } else {
+//      echo '3lla slamtak';
+//  }
+
+ ?>
